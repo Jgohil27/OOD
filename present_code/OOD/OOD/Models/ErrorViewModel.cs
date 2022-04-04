3bf6521287eca1f7bd118cd3a4619e5f00b12021
kalyank12 <kalyan.kollepara95@gmail.com>
2022-04-04 02:25:14 -0400

    present modified code

---
+++ present_code/OOD/OOD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OOD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
