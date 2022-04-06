16ac81337067a91d2a01ffbe3c85d91c76c25457
kalyank12 <kalyan.kollepara95@gmail.com>
2022-04-06 03:03:10 -0400

    db changes

--- present_code/Database/Profile.sql
+++ present_code/Database/Profile.sql
@@ -1,15 +1,20 @@
 CREATE PROCEDURE [dbo].[FetchProfile]
 @EmailId  varchar(50) , 
-@FirstName varchar(50) Output , @LastName varchar(50) Output
+@FirstName varchar(50) Output , @LastName varchar(50) Output ,
+@Email varchar(50) Output , @PhoneNumber int Output
 AS
 BEGIN
    
-    select FirstName, LastName from AspNetUsers where Email = @EmailId
+    select @FirstName=FirstName, @LastName =LastName ,@Email=Email , @PhoneNumber=PhoneNumber  from AspNetUsers where Email = @EmailId
 END
 GO
 
+EXEC dbo.FetchProfile @EmailId="kalyanking12@gmail.com" , @FirstName Output, @LastName Output , Input 
+
 select * from AspNetUsers
 
+DROP PROCEDURE [FetchProfile]
+GO
 
 CREATE PROCEDURE [dbo].[UpdatePassword]
 @EmailId  varchar(50) , 
