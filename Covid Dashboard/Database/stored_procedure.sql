5b01236864fdea962654209dc2b297f72eb7ebd5
Jgohil27 <90415362+Jgohil27@users.noreply.github.com>
2022-04-11 19:22:17 -0400

    Update stored_procedure.sql

--- Covid Dashboard/Database/stored_procedure.sql
+++ Covid Dashboard/Database/stored_procedure.sql
@@ -14,50 +14,57 @@ begin
 End 
 
 /*Save My Form Procedure*/
-Create procedure [dbo].AddFormSymptoms
-(  
-   @HaveMedicalSymptoms bit,  
-   @MedicalCovidSymptoms varchar (255),  
-   @DateNoted datetime ,
-   @Temperature varchar(255) ,
-   @TakeAnyMedicine bit,
-   @MedicineName varchar(255),
-   @DoctorVisit bit,
-   @DoctorProfession varchar(255) ,
-   @HadInteraction bit,
-   @HadInteractioCS bit,
-   @InteractionCS varchar(255),
-   @isIPersonPostive bit , 
-   @CovidDatetime datetime , 
-   @HadOutings bit , 
-   @OutingCS varchar(255), 
-   @Outforfood bit ,  
-   @RiskResult varchar(255) ,
-   @RiskScore int ,
-   @RiskCalculatedDate datetime
-)  
-as  
-begin  
-   Insert into Form values( @HaveMedicalSymptoms ,  
-   @MedicalCovidSymptoms  ,  
-   @DateNoted ,
-   @Temperature,
-   @TakeAnyMedicine ,
-   @MedicineName ,
-   @DoctorVisit ,
-   @DoctorProfession  ,
-   @HadInteraction ,
-   @HadInteractioCS ,
-   @InteractionCS ,
-   @isIPersonPostive  , 
-   @CovidDatetime  , 
-   @HadOutings  , 
-   @OutingCS, 
-   @Outforfood  ,  
-   @RiskResult ,
-   @RiskScore,
-   @RiskCalculatedDate ) 
-End 
+Create procedure [dbo].AddFormSymptoms  
+(    
+   @HaveMedicalSymptoms bit,    
+   @MedicalCovidSymptoms varchar (255),    
+   @DateNoted datetime ,  
+   @Temperature varchar(255) ,  
+   @TakeAnyMedicine bit,  
+   @MedicineName varchar(255),  
+   @DoctorVisit bit,  
+   @DoctorProfession varchar(255) ,  
+   @HadInteraction bit,  
+   @HadInteractioCS bit,  
+   @InteractionCS varchar(255),  
+   @isIPersonPostive bit ,   
+   @CovidDatetime datetime ,   
+   @HadOutings bit ,   
+   @OutingCS varchar(255),   
+   @Outforfood bit ,    
+   @RiskResult varchar(255) ,  
+   @RiskScore int ,  
+   @RiskCalculatedDate datetime , 
+   @HadVaccine bit , 
+   @VaccineDose int , 
+   @VaccineName varchar(100)
+)    
+as    
+begin    
+   Insert into Form values( @HaveMedicalSymptoms ,    
+   @MedicalCovidSymptoms  ,    
+   @DateNoted ,  
+   @Temperature,  
+   @TakeAnyMedicine ,  
+   @MedicineName ,  
+   @DoctorVisit ,  
+   @DoctorProfession  ,  
+   @HadInteraction ,  
+   @HadInteractioCS ,  
+   @InteractionCS ,  
+   @isIPersonPostive  ,   
+   @CovidDatetime  ,   
+   @HadOutings  ,   
+   @OutingCS,   
+   @Outforfood  ,    
+   @RiskResult ,  
+   @RiskScore,  
+   @RiskCalculatedDate,
+   @HadVaccine , 
+   @VaccineDose , 
+   @VaccineName 
+   )   
+End
 
 /*Fetch My Profile Procedure*/
 CREATE PROCEDURE [dbo].[FetchProfile]
